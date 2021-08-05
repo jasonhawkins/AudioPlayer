85d41b99055109c4303d8da8bf59aad621626e0e
Jason Hawkins <hellojason@me.com>
2021-08-05 14:45:36 -0400

    Initial Commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Jason Hawkins on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
